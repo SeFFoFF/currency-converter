57778902fc1d9c5c0f2bc0df00f4f9eb4586a911
Anton Yefimov <anton.yefimov19@gmail.com>
2022-09-02 16:42:29 +0300

    changed currency API

--- src/hooks/useCurrencyRate.js
+++ src/hooks/useCurrencyRate.js
@@ -4,25 +4,13 @@ export const useCurrencyRate = () => {
     const [currencyRate, setCurrencyRate] = useState({ usd: 0, eur: 0 })
 
     useEffect(() => {
-        const myHeaders = new Headers()
-        myHeaders.append("apikey", "s62visFsFzeU6D6oIYOgKESFADAHQtsD")
-
-        const requestOptions = {
-            method: "GET",
-            redirect: "follow",
-            headers: myHeaders
-        }
-
         const fetchData = async () => {
-            const usdData = await fetch("https://api.apilayer.com/exchangerates_data/convert?to=UAH&from=USD&amount=1", requestOptions)
-            const usdJson = await usdData.json()
-
-            const eurData = await fetch("https://api.apilayer.com/exchangerates_data/convert?to=UAH&from=EUR&amount=1", requestOptions)
-            const eurJson = await eurData.json()
+            const data = await fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+            const json = await data.json()
 
             setCurrencyRate({
-                usd: usdJson.info.rate.toFixed(2),
-                eur: eurJson.info.rate.toFixed(2)
+                usd: Number(json[0].buy).toFixed(2),
+                eur: Number(json[1].buy).toFixed(2)
             })
         }
 
